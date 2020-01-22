5c6b166324ccb14e3d3a3506dbdf936a19ab03e8
Przemyslaw Mejna <pmejna@gmail.com>
2020-01-22 21:50:15 +0000

    initial committ

---
+++ db.js
@@ -0,0 +1,81 @@
+const locatIon = ["Anubirath", "Kerras", "Teraa"];
+const races = {
+  Human: {
+    hpMax: 100,
+    manaMax: 100,
+    energy: 100,
+    str: 10,
+    dex: 10,
+    int: 10,
+    spd: 10,
+    end: 10,
+    def: 10,
+    physRes: 2,
+    poisonRes: 0,
+    fireRes: 0,
+    coldRes: 2,
+    electRes: 0,
+    magicRes: 0
+  },
+  Darghar: {
+    hpMax: 150,
+    manaMax: 80,
+    energy: 80,
+    str: 10,
+    dex: 10,
+    int: 10,
+    spd: 10,
+    end: 10,
+    def: 10,
+    physRes: 2,
+    poisonRes: 0,
+    fireRes: 0,
+    coldRes: 2,
+    electRes: 0,
+    magicRes: 0
+  },
+  Dekhij: {
+    hpMax: 120,
+    manaMax: 130,
+    energy: 60,
+    str: 10,
+    dex: 10,
+    int: 10,
+    spd: 10,
+    end: 10,
+    def: 10,
+    physRes: 2,
+    poisonRes: 0,
+    fireRes: 0,
+    coldRes: 2,
+    electRes: 0,
+    magicRes: 0
+  },
+  Anakis: {
+    hpMax: 80,
+    manaMax: 140,
+    energy: 70,
+    str: 10,
+    dex: 10,
+    int: 10,
+    spd: 10,
+    end: 10,
+    def: 10,
+    physRes: 2,
+    poisonRes: 0,
+    fireRes: 0,
+    coldRes: 2,
+    electRes: 0,
+    magicRes: 0
+  }
+};
+const race = ["Human", "Darghar", "Dekhij", "Anakis"];
+const names = [
+  "Gerad",
+  "Atrer",
+  "Streer",
+  "Echon",
+  "Baghir",
+  "Osteslaw",
+  "Petro"
+];
---
+++ main.js
@@ -0,0 +1,95 @@
+let player, enemy1, enemy2, enemy3;
+
+class Character {
+  constructor(name, race) {
+    (this.name = name), (this.race = race);
+    this.str,
+      this.dex,
+      this.int,
+      this.spd,
+      this.end,
+      this.def,
+      this.lvl,
+      this.hpMax,
+      this.manaMax,
+      this.energy,
+      this.physRes,
+      this.poisonRes,
+      this.fireRes,
+      this.coldRes,
+      this.electRes,
+      this.magicRes;
+  }
+}
+// Basic random function for enemy creation
+
+const rndm = (char, num) => {
+  return Math.floor(Math.random() * (num / 100 + char / 2) + 1);
+};
+
+const createChar = name => {
+  player = new Character();
+  player.name = name;
+  player.race = race[Math.floor(Math.random() * race.length)];
+  player.hpMax = 100;
+};
+
+const randEnemy1 = () => {
+  let raceLngth = Object.keys(races).length;
+  let race = Object.keys(races)[Math.floor(Math.random() * raceLngth)];
+  enemy1 = new Character();
+  enemy1.name = names[Math.floor(Math.random() * names.length)];
+  enemy1.race = `${race}`;
+  enemy1.lvl = Math.floor(Math.random() * 100) + 1;
+  enemy1.str = (races[race].str / 2) * rndm(enemy1.lvl, races[race].str);
+  enemy1.dex = (races[race].dex / 2) * rndm(enemy1.lvl, races[race].dex);
+  enemy1.int = (races[race].int / 2) * rndm(enemy1.lvl, races[race].int);
+  enemy1.spd = (races[race].spd / 2) * rndm(enemy1.lvl, races[race].spd);
+  enemy1.end = (races[race].end / 2) * rndm(enemy1.lvl, races[race].end);
+  enemy1.def = (races[race].def / 2) * rndm(enemy1.lvl, races[race].def);
+  enemy1.hpMax =
+    races[race].hpMax +
+    enemy1.end * 2 +
+    enemy1.def +
+    (enemy1.str + enemy1.dex) * 0.3;
+  enemy1.manaMax = races[race].manaMax;
+  enemy1.energy = races[race].manaMax;
+  enemy1.physRes = races[race].physRes;
+  enemy1.poisonRes = races[race].poisonRes;
+  enemy1.fireRes = races[race].fireRes;
+  enemy1.coldRes = races[race].coldRes;
+  enemy1.electRes = races[race].electRes;
+  enemy1.magicRes = races[race].magicRes;
+};
+
+const randEnemy2 = () => {
+  let raceLngth = Object.keys(races).length;
+  let race = Object.keys(races)[Math.floor(Math.random() * raceLngth)];
+  enemy2 = new Character();
+  enemy2.name = names[Math.floor(Math.random() * names.length)];
+  enemy2.race = `${race}`;
+  enemy2.lvl = Math.floor(Math.random() * 100) + 1;
+  enemy2.str = (races[race].str / 2) * rndm(enemy2.lvl, races[race].str);
+  enemy2.dex = (races[race].dex / 2) * rndm(enemy2.lvl, races[race].dex);
+  enemy2.int = (races[race].int / 2) * rndm(enemy2.lvl, races[race].int);
+  enemy2.spd = (races[race].spd / 2) * rndm(enemy2.lvl, races[race].spd);
+  enemy2.end = (races[race].end / 2) * rndm(enemy2.lvl, races[race].end);
+  enemy2.def = (races[race].def / 2) * rndm(enemy2.lvl, races[race].def);
+  enemy2.hpMax =
+    races[race].hpMax +
+    enemy2.end * 2 +
+    enemy2.def +
+    (enemy2.str + enemy2.dex) * 0.3;
+  enemy2.manaMax = races[race].manaMax;
+  enemy2.energy = races[race].manaMax;
+  enemy2.physRes = races[race].physRes;
+  enemy2.poisonRes = races[race].poisonRes;
+  enemy2.fireRes = races[race].fireRes;
+  enemy2.coldRes = races[race].coldRes;
+  enemy2.electRes = races[race].electRes;
+  enemy2.magicRes = races[race].magicRes;
+};
+randEnemy1();
+randEnemy2();
+console.log(enemy1);
+console.log(enemy2);
