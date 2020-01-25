57ea876b4e243559f6093d83fbcf71b2c70e889b
Przemyslaw Mejna <pmejna@gmail.com>
2020-01-25 14:44:21 +0000

    add gsap animation on click

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
+// variables
 let player, enemy1, enemy2, enemy3;
-const start = document.querySelector(".start");
+const start = document.querySelector(".main");
 
+// functions
+const fadeOut = function(target) {
+  gsap.to(target, 1, {
+    x: -300,
+    stagger: 0.6,
+    ease: Power2.easeOut
+  });
+};
+const removClass = (target, className) => target.classList.remove(className);
+
+// active functions
+start.addEventListener("click", function() {
+  fadeOut(start);
+});
 randEnemy1();
 randEnemy2();
+
 console.log(enemy1);
 console.log(enemy2);
